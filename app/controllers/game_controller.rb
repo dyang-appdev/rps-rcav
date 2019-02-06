bacad77b76dd969438cc3cde6b1fce90346f35b6
Diana Yang <dyang50@chicagobooth.edu>
2019-02-06 05:37:38 +0000

    partially_completed_game
    includes randomized generation of computer hand, need to update icons to match what computer plays.

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,54 @@
+class GameController < ApplicationController
+  def user_plays_rock
+      
+    @computer_move = ["rock", "paper", "scissors"].sample
+      
+    if @computer_move == "rock"
+        @outcome = "tied"
+        @image = "far fa-hand-rock fa-10x"
+    elsif @computer_move == "paper"
+        @outcome = "lost"
+        @image = "far fa-hand-paper fa-10x"
+    else 
+        @outcome = "won"
+        @image = "far fa-hand-scisssors fa-10x"
+    end
+
+    render("move_templates/play_rock.html.erb")
+  end
+  
+  def user_plays_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+      
+    if @computer_move == "rock"
+        @outcome = "won"
+        @image = "far fa-hand-rock fa-10x"
+    elsif @computer_move == "paper"
+        @outcome = "tied"
+        @image = "far fa-hand-paper fa-10x"
+    else 
+        @outcome = "lost"
+        @image = "far fa-hand-scisssors fa-10x"
+    end
+
+    render("move_templates/play_paper.html.erb")
+  end
+     
+  def user_plays_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+      
+    if @computer_move == "rock"
+        @outcome = "lost"
+        @image = "far fa-hand-rock fa-10x"
+    elsif @computer_move == "paper"
+        @outcome = "won"
+        @image = "far fa-hand-paper fa-10x"
+    else 
+        @outcome = "tied"
+        @image = "far fa-hand-scisssors fa-10x"
+    end
+
+    render("move_templates/play_scissors.html.erb")
+  end
+       
+end
